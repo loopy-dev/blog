6b879ff936179b8a732ed9daa4a5eda73ee763a0
Junhyuck Ko <mrbartrns@naver.com>
2023-01-20 13:58:48 +0900

    chore: move directory

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default Home;
