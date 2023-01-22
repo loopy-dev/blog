9d161ea34b965d06c68536580d5b894454b87041
Junhyuck Ko <mrbartrns@naver.com>
2023-01-22 16:39:48 +0900

    feat: add props on project component and add data

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,12 +1,21 @@
 // TODO - prop으로 데이터 전달받아서 컴포넌트 재사용
+import Link from "next/link";
+import { Project } from "../../models/Project";
 import classes from "./Project.module.css";
 
-const Project = () => {
+interface Props {
+  project: Project;
+}
+
+const Project = ({ project }: Props) => {
   return (
     <div className="flex relative md:flex-row flex-col gap-2 w-full">
       <div className="md:sticky self-start top-1 md:w-60 shrink-0">
-        <h2 className="font-bold text-3xl py-1 break-all">Title</h2>
-        <p className="pt-4 text-zinc-400">2022-10 ~ </p>
+        <h2 className="font-bold text-3xl py-1 break-all">{project.title}</h2>
+        {/** TODO - Date format 후 사용하기 */}
+        <p className="pt-4 text-zinc-400">
+          {project.startDate} ~ {project.endDate || ""}
+        </p>
         <div className="flex gap-2 pt-4 mb-4">
           <div className="bg-zinc-300 w-8 h-8" />
           <div className="bg-zinc-300 w-8 h-8" />
@@ -15,15 +24,19 @@ const Project = () => {
       </div>
       <div className="w-full">
         {/** Project Image */}
-        <h3 className="font-bold text-xl mb-4">
-          퀴즈를 통한 지식 체크 서비스를 제공합니다.퀴즈를 통한 지식 체크
-          서비스를 제공합니다.퀴즈를 통한 지식 체크 서비스를 제공합니다.
-        </h3>
-        <ul className="my-4">
-          <li>세부 사항 1</li>
-          <li>세부 사항 2</li>
-          <li>세부 사항 3</li>
-          <li>세부 사항 4</li>
+        <h3 className="font-bold text-xl mb-4">{project.introduction}</h3>
+        <ul className="my-4 list-disc pl-4">
+          {project.description.map((description) =>
+            typeof description === "string" ? (
+              <li key={description} className="py-1">
+                {description}
+              </li>
+            ) : (
+              <li key={description.name} className="py-1">
+                {description.name}
+              </li>
+            )
+          )}
         </ul>
         {/** 기술 스택 */}
         <div className="flex items-center gap-8">
@@ -35,24 +48,32 @@ const Project = () => {
         <div
           className={`flex gap-2 my-4 overflow-x-auto ${classes["scroll-hide"]}`}
         >
-          <div className="border py-1 px-4 rounded-full cursor-pointer hover:bg-rose-400 hover:text-white select-none transition-all whitespace-nowrap">
-            React
-          </div>
-          <div className="border py-1 px-4 rounded-full cursor-pointer hover:bg-rose-400 hover:text-white select-none transition-all whitespace-nowrap">
-            Tailwind css
-          </div>
-          <div className="border py-1 px-4 rounded-full cursor-pointer hover:bg-rose-400 hover:text-white select-none transition-all whitespace-nowrap">
-            Next
-          </div>
-          <div className="border py-1 px-4 rounded-full cursor-pointer hover:bg-rose-400 hover:text-white select-none transition-all whitespace-nowrap">
-            Styled Components
-          </div>
+          {project.skills.map((skill) => (
+            <div
+              key={skill.name}
+              className="border py-1 px-4 rounded-full cursor-pointer hover:bg-rose-400 hover:text-white select-none transition-all whitespace-nowrap"
+            >
+              {skill.name}
+            </div>
+          ))}
         </div>
         <div>
-          <h4 className="font-bold text-lg my-2">React</h4>
-          <p>사용 기술에 대한 설명입니다.</p>
+          {/** TODO - 현재 선택한 스택을 기준으로 표시하기 */}
+          <h4 className="font-bold text-lg my-2">{project.skills[0].name}</h4>
+          <p>{project.skills[0].description}</p>
         </div>
-        <h3 className="font-bold text-xl my-4">링크</h3>
+        {project.otherLinks ? (
+          <h3 className="font-bold text-xl my-4">링크</h3>
+        ) : null}
+        {project.otherLinks ? (
+          <ul className="list-disc pl-4">
+            {project.otherLinks.map((link) => (
+              <li key={link.url} className="py-1">
+                <Link href={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        ) : null}
         Project Component
         <p>long text</p>
         <p>long text</p>
