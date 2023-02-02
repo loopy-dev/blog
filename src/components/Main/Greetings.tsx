9759e5452f06704c24e71e3c6f63a61c4e2335de
Junhyuck Ko <mrbartrns@naver.com>
2023-02-02 14:51:24 +0900

    feat: create Slider

--- src/components/Main/Greetings.tsx
+++ src/components/Main/Greetings.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react';
-import useIntersection from '../../hooks/common/useIntersection';
-import classNames from './Greetings.module.css';
+import FadeInUp from '../common/FadeInUp';
+import Slider from './Slider';
+
+const characteristics = [
+  '호기심 많은',
+  '글 쓰는 것을 좋아하는',
+  '항상 최선을 다하는',
+];
 
 const Greetings = () => {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-
-  const ref = useIntersection<HTMLDivElement>(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && ref.current) {
-          setIsIntersecting(true);
-          observer.unobserve(ref.current);
-        }
-      });
-    },
-    { threshold: 0.5 }
-  );
-
   return (
-    <div
-      ref={ref}
-      className={`${
-        isIntersecting ? classNames.greetings : 'opacity-0'
-      } my-24 font-bold text-7xl leading-normal`}
-    >
-      <h1>안녕하세요,</h1>
-      <h1>저는</h1>
-      <h1>____한 개발자</h1>
-      <h1>고준혁 입니다.</h1>
-    </div>
+    <FadeInUp>
+      <div className="my-24 font-bold text-7xl leading-normal">
+        <h1>안녕하세요,</h1>
+        <h1>저는</h1>
+        <h1>
+          <Slider items={characteristics} /> 개발자
+        </h1>
+        <h1>고준혁 입니다.</h1>
+      </div>
+    </FadeInUp>
   );
 };
 
