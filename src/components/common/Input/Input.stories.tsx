20afc00f64cde4e6e4926a8f0ff7ff0dfca86347
Junhyuck Ko <mrbartrns@naver.com>
2023-02-08 15:10:39 +0900

    feat: create Input component

---
+++ src/components/common/Input/Input.stories.tsx
@@ -0,0 +1,8 @@
+import Input from './Input';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Components/Input',
+} as ComponentMeta<typeof Input>;
+
+export const Default: ComponentStory<typeof Input> = () => <Input />;
