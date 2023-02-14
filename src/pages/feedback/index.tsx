8898c9e89f9ac144e2206df17de753ad315ffc0a
Junhyuck Ko <mrbartrns@naver.com>
2023-02-14 13:50:18 +0900

    feat: apply firebase collection

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,17 +1,34 @@
 import Head from 'next/head';
 import { useForm } from 'react-hook-form';
+import { postFeedback } from '../../api/feeback';
 import Button from '../../components/common/Button';
 import Input from '../../components/common/Input';
 import TextArea from '../../components/common/TextArea';
 import ContentLayout from '../../components/layouts/ContentLayout';
 import GlobalLayout from '../../components/layouts/GlobalLayout';
+import type { FeebackForm } from '../../models/Feeback';
+import type { SubmitHandler } from 'react-hook-form/dist/types';
 
 const Page = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit: SubmitHandler<FeebackForm> = async (data) => {
+    try {
+      await postFeedback(data);
+      // alert user
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const { register, handleSubmit: onSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit: onSubmit,
+    formState,
+  } = useForm<FeebackForm>({
+    defaultValues: {
+      email: '',
+      description: '',
+    },
+  });
 
   return (
     <>
@@ -28,12 +45,9 @@ const Page = () => {
             </h2>
           </header>
           <form
-            onSubmit={onSubmit(
-              (data) => console.log(data),
-              (invalid) => {
-                console.log(invalid);
-              }
-            )}
+            onSubmit={onSubmit(async (data) => {
+              await handleSubmit(data);
+            })}
           >
             <section className="flex flex-col gap-8 bg-white rounded-md md:border drop-shadow-sm hover:border-gray-600 transition-all md:p-8">
               <div className="flex flex-col gap-2">
@@ -56,17 +70,17 @@ const Page = () => {
               </div>
               <div className="flex flex-col gap-2">
                 <TextArea
-                  id="feedback"
+                  id="description"
                   label="피드백"
                   placeholder="피드백을 적어주세요."
                   rows={10}
-                  {...register('feedback', {
+                  {...register('description', {
                     required: true,
                     minLength: 3,
                   })}
-                  variant={formState.errors.feedback ? 'error' : 'default'}
+                  variant={formState.errors.description ? 'error' : 'default'}
                 />
-                {formState.errors.feedback && (
+                {formState.errors.description && (
                   <p className="text-red-500 text-sm">
                     최소 세 글자 이상 입력해야 합니다.
                   </p>
