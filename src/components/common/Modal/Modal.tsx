77a72b76562543fcfcccc8ff706fe4af783d69f9
Junhyuck Ko <mrbartrns@naver.com>
2023-02-20 22:33:23 +0900

    feat: create Modal base

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,4 +1,29 @@
-const Modal = () => {
+import { useEffect, useMemo } from 'react';
+import ReactDOM from 'react-dom';
+import { useModalContext } from './ModalContext';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const Modal = ({ children }: Props) => {
+  const { modalState, close } = useModalContext();
+
+  const portal = useMemo(() => {
+    const element = document.createElement('div');
+    element.className = 'modal';
+
+    return element;
+  }, []);
+
+  useEffect(() => {
+    document.body.appendChild(portal);
+
+    return () => {
+      document.body.removeChild(portal);
+    };
+  }, [portal]);
+
   return <div />;
 };
 
