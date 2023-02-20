6ed88769de6dd0fac71dee23168e1f9e6681e2fc
Junhyuck Ko <mrbartrns@naver.com>
2023-02-20 22:24:43 +0900

    feat: register Modal context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { Analytics } from '@vercel/analytics/react';
+import { ModalProvider } from '~/components/common/Modal';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ModalProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </ModalProvider>
   );
 };
 
