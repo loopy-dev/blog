182104be02ef44627b915d1835e763b7ddd9e7da
Junhyuck Ko <mrbartrns@naver.com>
2023-02-27 11:40:48 +0900

    feat: create useClickAway

---
+++ src/hooks/common/useClickAway/useClickAway.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+const useClickAway = <T extends HTMLElement>(
+  onClick?: (e: MouseEvent) => void | Promise<void>
+) => {
+  const elementRef = useRef<T | null>(null);
+
+  const handleClick = useCallback(
+    (e: MouseEvent) => {
+      const { current } = elementRef;
+
+      if (!current) return;
+
+      if (!current.contains(e.target as Element)) onClick?.(e);
+    },
+    [onClick]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+};
+
+export default useClickAway;
