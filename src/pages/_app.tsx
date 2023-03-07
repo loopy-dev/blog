a580f1a165c3d1596536512a2a4baafb828c6893
Junhyuck Ko <mrbartrns@naver.com>
2023-03-07 23:05:30 +0900

    chore: font optimization with fallback fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { Analytics } from '@vercel/analytics/react';
 import { ModalProvider } from '~/components/common/Modal';
+import { Pretendard } from '~/styles/fonts/Pretendard';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ModalProvider>
-      <Component {...pageProps} />
+      <div className={Pretendard.className}>
+        <Component {...pageProps} />
+      </div>
       <Analytics />
     </ModalProvider>
   );
