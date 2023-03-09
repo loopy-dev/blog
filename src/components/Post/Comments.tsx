ec06a8ed37f06b02438bfc71efa20a3649182755
Junhyuck Ko <mrbartrns@naver.com>
2023-03-09 11:29:15 +0900

    feat: add comment

---
+++ src/components/Post/Comments.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+// Comment system uses github utterances.
+const Comments = () => {
+  const commentRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const $script = document.createElement('script');
+    $script.async = true;
+    $script.crossOrigin = 'anonymous';
+    $script.setAttribute('src', 'https://utteranc.es/client.js');
+    $script.setAttribute('issue-term', 'title');
+    $script.setAttribute('label', 'Comment');
+    $script.setAttribute('theme', 'github-light');
+    $script.setAttribute('repo', 'mrbartrns/portfolio');
+
+    commentRef.current?.appendChild($script);
+  }, []);
+  return <Section ref={commentRef} />;
+};
+
+export default Comments;
+
+const Section = styled.section`
+  margin-top: 1.5rem;
+`;
