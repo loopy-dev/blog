f5c68ca0f6db2372109a9a08c41357d23f5d8d3a
Junhyuck Ko <mrbartrns@naver.com>
2023-03-09 16:06:56 +0900

    refactor(NavigationBar): style

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,60 +1,29 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
-import Icon from '../icons';
-import classes from './NavigationBar.module.scss';
 
 // TODO - show floating menu button when display size is under sm
 const NavigationBar = () => {
   return (
     <Container>
-      {/** top */}
-      <div className="flex">
-        <Link
-          className={`${classes['navbar-icon']} flex justify-center items-center w-16 h-16 hover:bg-gray-300 transition-all rounded-lg`}
-          href="/"
-        >
-          <div className="m-2">
-            <Image
-              priority
-              alt="profile"
-              className="cursor-pointer"
-              height={64}
-              src="https://avatars.githubusercontent.com/u/56826914?v=4"
-              width={64}
-            />
-          </div>
-        </Link>
-        <Link
-          className={`${classes['navbar-icon']} flex justify-center items-center w-16 h-16 hover:bg-gray-300 transition-all rounded-lg`}
-          href="/posts"
-        >
-          <Icon type="blog" />
-        </Link>
-      </div>
-      {/** bottom */}
-      <div className="flex">
-        <a
-          className={`${classes['navbar-icon']} flex justify-center items-center w-16 h-16 hover:bg-gray-300 transition-all rounded-lg`}
-          href="https://github.com/mrbartrns"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Icon type="github" />
-        </a>
-        <a
-          className={`${classes['navbar-icon']} flex justify-center items-center w-16 h-16 hover:bg-gray-300 transition-all rounded-lg`}
-          href="mailto:mrbartrns@naver.com"
-        >
-          <Icon type="mail" />
-        </a>
-        <Link
-          className={`${classes['navbar-icon']} flex justify-center items-center w-16 h-16 hover:bg-gray-300 transition-all rounded-lg`}
-          href="/feedback"
-        >
-          <Icon type="project" />
-        </Link>
-      </div>
+      <ContentContainer>
+        <ItemWrapper className="left">
+          <Link href="/">
+            <Item>Benlog</Item>
+          </Link>
+        </ItemWrapper>
+        <ItemWrapper className="right">
+          <Link href="/posts">
+            <Item>Blog</Item>
+          </Link>
+          <Link
+            href="https://github.com/mrbartrns"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <Item>Github</Item>
+          </Link>
+        </ItemWrapper>
+      </ContentContainer>
     </Container>
   );
 };
@@ -62,11 +31,8 @@ const NavigationBar = () => {
 export default NavigationBar;
 
 const Container = styled.nav`
-  display: flex;
-  justify-content: space-between;
   box-shadow: inset 0 -1px 0 0 rgba(0, 0, 0, 0.1);
   background-color: hsla(0, 0%, 100%, 0.8);
-  backdrop-filter: saturate(180%) blur(5px);
   z-index: 99;
   transition: all 0.1s;
 
@@ -74,5 +40,36 @@ const Container = styled.nav`
     position: sticky;
     top: 0;
     backdrop-filter: saturate(180%) blur(5px);
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 56rem;
+  padding: 8px 24px;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    .right {
+      display: none;
+    }
+  }
+`;
+
+const ItemWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  padding: 8px 16px;
+  border-radius: 4px;
+  user-select: none;
+  transition: all 100ms cubic-bezier(0.31, 0.27, 0.15, 0.99) 0s;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.06) 0px 0px 0px 10000px inset;
   }
 `;
