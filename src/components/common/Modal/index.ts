eb6f69b5c5292f17d17d7f948acceb3f39fa5ccc
Junhyuck Ko <mrbartrns@naver.com>
2023-03-16 09:23:59 +0900

    fix: build issues

--- src/components/common/Modal/index.ts
+++ src/components/common/Modal/index.ts
@@ -1,5 +1,3 @@
 import ModalBackground from './Modal';
-import ModalProvider, { useModalContext } from './ModalContext';
 
-export { ModalProvider, useModalContext };
 export default ModalBackground;
