3ab6993370ca5cf8ab6c866f683bf8584ed45c99
Junhyuck Ko <mrbartrns@naver.com>
2023-04-04 17:01:53 +0900

    refactor(ThemeScript): iife

--- src/lib/styles/ThemeScript.tsx
+++ src/lib/styles/ThemeScript.tsx
@@ -1,14 +1,16 @@
 const ThemeScript = () => {
   const setTheme = `
-    function getInitTheme() {
-      const theme = window.localStorage.getItem('theme');
-      const systemPrefersDark = window.matchMedia(
-        '(prefers-color-scheme: dark)'
-      ).matches;
-      return theme ? theme : systemPrefersDark ? 'dark' : 'light'
-    }
+    (function () {
+      function getInitTheme() {
+        const theme = window.localStorage.getItem('theme');
+        const systemPrefersDark = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        ).matches;
+        return theme ? theme : systemPrefersDark ? 'dark' : 'light';
+      }
 
-    document.body.dataset.theme = getInitTheme();
+      document.body.dataset.theme = getInitTheme();
+    })();
   `;
   return <script dangerouslySetInnerHTML={{ __html: setTheme }} />;
 };
