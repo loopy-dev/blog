49a8bddb2be01667f036af5b173ed3c57b43f409
Junhyuck Ko <mrbartrns@naver.com>
2023-04-19 22:25:42 +0900

    refactor: ArticleLayout

--- src/components/layouts/ArticleLayout.tsx
+++ src/components/layouts/ArticleLayout.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import ContentLayout from './ContentLayout';
 
 interface Props {
@@ -8,10 +9,10 @@ interface Props {
 const ArticleLayout = ({ title, children }: Props) => {
   return (
     <ContentLayout>
-      <div>
-        <h1 className="font-bold text-3xl my-8 break-all">{title}</h1>
-      </div>
-      <div className="flex flex-col gap-40">{children}</div>
+      <h1 className={classNames('font-bold', 'text-3xl', 'my-8', 'break-all')}>
+        {title}
+      </h1>
+      <div className={classNames('flex', 'flex-col', 'gap-40')}>{children}</div>
     </ContentLayout>
   );
 };
