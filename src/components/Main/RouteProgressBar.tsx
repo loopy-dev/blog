ecd83bb860202858c699d0adf6988bde3dd2c69e
Junhyuck Ko <mrbartrns@naver.com>
2023-05-23 14:31:03 +0900

    feat: create RouteProgressBar

---
+++ src/components/Main/RouteProgressBar.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import ProgressBar from '~components/common/ProgressBar/ProgressBar';
+import styles from './RouteProgressBar.module.scss';
+
+const RouteProgressBar = () => {
+  const router = useRouter();
+  const [progress, setProgress] = useState(0);
+  const [className, setClassName] = useState('');
+
+  useEffect(() => {
+    const handleStart = () => {
+      setClassName('progress-bar__phase-1');
+      setProgress(0);
+    };
+
+    const handleChange = () => {
+      const randomInt = Math.floor(Math.random() * 51 + 30);
+
+      setProgress(randomInt);
+
+      // setTimeout을 이용하여 애니메이션 처리 가능
+      window.setTimeout(() => {
+        setClassName('progress-bar__phase-2');
+        setProgress(100);
+      }, 750);
+
+      window.setTimeout(() => {
+        setClassName('');
+        setProgress(0);
+      }, 1000);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleChange);
+    router.events.on('routeChangeError', handleChange);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleChange);
+      router.events.off('routeChangeError', handleChange);
+    };
+  }, [router.events]);
+
+  return (
+    <ProgressBar className={classNames(styles[className])} width={progress} />
+  );
+};
+
+export default RouteProgressBar;
--- src/components/common/ProgressBar/ProgressBar.tsx
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -2,9 +2,10 @@ import classNames from 'classnames';
 
 interface Props {
   width?: number;
+  className?: string;
 }
 
-const ProgressBar = ({ width = 0 }: Props) => {
+const ProgressBar = ({ width = 0, className }: Props) => {
   const barWidth = width < 0 ? 0 : width > 100 ? 100 : width;
 
   return (
@@ -12,11 +13,12 @@ const ProgressBar = ({ width = 0 }: Props) => {
       style={{ width: `${barWidth}%` }}
       className={classNames(
         'fixed',
-        'h-1',
+        'h-0.5',
         'top-0',
         'left-0',
         'bg-[color:var(--primary)]',
-        'z-[100]'
+        'z-[100]',
+        className
       )}
     />
   );
