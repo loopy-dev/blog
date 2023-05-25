a1d3cd9090e65e8d52b457ddf299f6f516cd48b7
Junhyuck Ko <56826914+mrbartrns@users.noreply.github.com>
2023-05-25 20:19:12 +0900

    fix(PostFooter): copy message (#164)
    * fix(PostFooter): copy message
    
    * fix: base64 encode all urls

--- src/pages/api/post/hits/[url].ts
+++ src/pages/api/post/hits/[url].ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Base64 } from 'js-base64';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { PostHits } from '~models/Post';
 
@@ -19,7 +20,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     });
   }
 
-  const url = `https://hits.dwyl.com/${process.env.GITHUB_ID}/${process.env.POST_PREFIX}-${pathname}.json`;
+  const encoded = Base64.encodeURI(
+    typeof pathname === 'string' ? pathname : pathname[0]
+  );
+
+  const url = `https://hits.dwyl.com/${process.env.GITHUB_ID}/${encoded}.json`;
 
   const response = await axios.get<PostHits>(url);
 
