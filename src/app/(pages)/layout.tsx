34cdee3dd733ffa74266c98b7d6c447c321bdc4a
Junhyuck <mrbartrns@naver.com>
2023-12-17 15:14:19 +0900

    chore: update layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
+import { DEFAULT_PAGE_DESCRIPTION, DEFAULT_PAGE_TITLE } from '~lib/constants';
+import { pageview, GA_TRACKING_ID } from '~/lib/ga/gtag';
 import StyledComponentsRegistry from '~lib/registry/styled-components';
 import '~lib/styles/globals.scss';
 
 export const metadata: Metadata = {
-  title: 'Belong',
-  description: 'Benlog입니다.',
+  title: DEFAULT_PAGE_TITLE,
+  description: DEFAULT_PAGE_DESCRIPTION,
 };
 
 interface Props {
@@ -20,6 +23,35 @@ const Layout = ({ children }: Props) => {
           crossOrigin=""
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
           rel="stylesheet"
+        />
+        <title>{(metadata.title as string) || DEFAULT_PAGE_TITLE}</title>
+        <meta
+          key="title"
+          content={(metadata.title as string) || DEFAULT_PAGE_TITLE}
+          property="og:title"
+        />
+        <meta
+          key="description"
+          content={metadata.description ?? DEFAULT_PAGE_DESCRIPTION}
+          property="og:description"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname
+        });
+      `,
+          }}
+        />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          strategy="afterInteractive"
         />
       </head>
       <body>
